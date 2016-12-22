efd0aa3763e1a0c8c0695fcf1c5117855fcc3c7e
Jared Swenson <jswen15@gmail.com>
2016-12-22 10:10:18 -0700

    log in and sign up style

--- app/controllers/players/registrations_controller.rb
+++ app/controllers/players/registrations_controller.rb
@@ -2,32 +2,32 @@ class Players::RegistrationsController < Devise::RegistrationsController
 before_action :configure_sign_up_params, only: [:create]
 before_action :configure_account_update_params, only: [:update]
 
-  GET /resource/sign_up
+  # GET /resource/sign_up
   def new
     super
   end
 
-  POST /resource
+  # POST /resource
   def create
     super
   end
 
-  GET /resource/edit
+  # GET /resource/edit
   def edit
     super
   end
 
-  PUT /resource
+  # PUT /resource
   def update
     super
   end
 
-  DELETE /resource
+  # DELETE /resource
   def destroy
     super
   end
 
-  GET /resource/cancel
+  # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
@@ -48,7 +48,7 @@ before_action :configure_account_update_params, only: [:update]
     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   end
 
-  The path used after sign up.
+  # The path used after sign up.
   def after_sign_up_path_for(resource)
     super(resource)
   end
