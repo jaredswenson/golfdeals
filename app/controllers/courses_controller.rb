81a8652c50210e43ae9273c97de101597c0ddb55
Brad Flint <bradwflint@gmail.com>
2016-12-22 17:32:13 -0700

    fixed errors

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,7 @@ class CoursesController < ApplicationController
   def index
   	if course_signed_in?
   		@course = current_course
+  		@teetimes = Teetime.new
   	end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'players/index'
-
-  get 'players/show'
-
-  get 'courses/index'
-
-  get 'courses/show'
-
   devise_for :courses, controllers: {
         sessions: 'courses/sessions',
         registrations: "courses/registrations"
@@ -18,7 +10,7 @@ Rails.application.routes.draw do
         registrations: "players/registrations"
       }
 
-      get '/' => 'home#index'
+      resources :teetimes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "home#index"
