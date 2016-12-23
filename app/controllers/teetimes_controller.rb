47a59223a6db661ec5d30207748aa64c5ffec296
Brad Flint <bradwflint@gmail.com>
2016-12-23 11:37:49 -0700

    create discount bug fix

--- app/controllers/teetimes_controller.rb
+++ app/controllers/teetimes_controller.rb
@@ -1,2 +1,18 @@
 class TeetimesController < ApplicationController
+
+	def create
+    @teetime = current_course.teetimes.new(teetime_params)
+    if @teetime.save
+        redirect_to root_path
+    else
+      render "new"
+    end
+   end
+
+    private
+	def teetime_params
+	    params.require(:teetime).permit(:course_id, :cost, :starttime, :endtime, :holes)
+	end
 end
+
+
